c0c27ad503d0d7505b663e02622914e996b0f3bb
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2005-06-19 16:45:43 +0000

    - ACBase: changed some methods names and behaviors  - ACBase: added render* methods

--- trunk/libs/action/controller/Base.php
+++ trunk/libs/action/controller/Base.php
@@ -32,10 +32,12 @@
 // ///////////////////////////////////////////////////////////////////////////////
 // }}}
 /**
- * @package locknet7.action.controller.base
+ * @package locknet7.action.controller
  */
     
 include_once('action/controller/Dependency.php');
+
+include_once('action/view/Base.php');
  
 
 /**
@@ -56,6 +58,10 @@ class ActionControllerBase {
     protected $session;
     /** values for template class */
     protected $assigns;
+    /** Default location for template files*/
+	protected $template_root;
+    /** Template Engine */
+	protected $template;
 
     /**
      * Will process the request returning the resulting response
@@ -64,9 +70,7 @@ class ActionControllerBase {
      * @return Response
      */
     public function process(Request $request, Response $response) {
-        // todo. start template.
-        $this->assign_shortcuts($request, $response);
-
+        $this->instantiate($request, $response);
         $this->add_before_filters();
         $this->add_models();
 
@@ -74,23 +78,44 @@ class ActionControllerBase {
         return $response;
     }
 
+	// {{{ render.
+	
+	protected function render($template_name = NULL, $status = NULL) {
+		if (is_null($template_name)) $template_name = $this->params['action'];
+		return $this->render_file($this->template_root . $template_name . '.phtml', $status);
+	}
+	
+	protected function render_file($template_file, $status) {
+		$this->logger->debug($template_file);
+		$this->render_text($this->template->render_file($template_file), $status);
+		
+	}
+	
+	protected function render_text($text, $status) {
+        if (is_null($status)) $status = Response::SC_OK;
+		$this->response->setStatus($status);
+		$this->response->setBody($text);
+	}
+	
+	// }}}
+
     /**
      * Act as an internal constructor.
      * @param Request request, the request
      * @param Response response, the response
      * @return void
      */
-    private function assign_shortcuts(Request $request, Response $response) {
+    private function instantiate(Request $request, Response $response) {
         $this->request  = $request;
         $this->response = $response;
         $this->logger   = Logger::getInstance();
         $this->session  = $request->getSession();
         $this->params   = $request->getParams();
-    }
-
-    // do we need smthing like this???
-    public function actionName() {
-        return isset($this->params['action']) ? $this->params['action'] : 'index';
+        $this->template_root = TOP_LOCATION . 'app' . DIRECTORY_SEPARATOR . 
+			 'views' . DIRECTORY_SEPARATOR . $this->params['controller'] . DIRECTORY_SEPARATOR;
+			 
+		$this->template = ActionViewBase::factory();
+		
     }
 
     // XXX: not-done!
@@ -102,15 +127,8 @@ class ActionControllerBase {
         // $options['filename'] = File->basename($location);
     }
 
-    // XXX: not-done
-    protected function sendData($data, $options = array()) {
-
-    }
-
     // XXX: not done.
-    protected function redirect() {
-
-    }
+    protected function redirect() { }
 
     /**
      * Performs the action
@@ -129,7 +147,7 @@ class ActionControllerBase {
         $this->logger->debug('Incoming action:: ' . strtolower($action_name));
         $action = $this->createMethod(strtolower($action_name));
         if ($action->isStatic()) throw new Exception('Cannot invoke a static method!');
-        return $action->invoke($this);
+        return $this->render($action->invoke($this));
     }
     
     /**
@@ -164,6 +182,10 @@ class ActionControllerBase {
         }
     }
 
+	/**
+	 * Injects model names into ActiveRecordBase by using the ModelInjector.
+	 * TODO: table inheritance.
+	 */
     private function add_models() {
         if (isset($this->model)) {
 
@@ -179,9 +201,18 @@ class ActionControllerBase {
         }
     }
     
+    /** 
+     * By using the php Reflection API we create 
+     * in a safty way the method with the name $method_name on this object
+     * 
+     * This method is used to perform the actions given by before/pre filters
+     * and also when we perform the action
+     * @param string method_name, the method.
+     * 							  NOTE: We force the name to be on lower case.
+     * @return RelfectionMethod
+     * @throws RelfectionException in case of failure. 
+     */ 
     private function createMethod($method_name) {
         return new ReflectionMethod($this, strtolower($method_name));
     }
-    
-
 }
