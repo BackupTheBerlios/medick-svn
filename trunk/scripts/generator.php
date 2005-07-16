bdfb86e8c1b1f5f5d39f894ae2fbdb1f2a80feec
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2005-07-16 15:24:16 +0000

    - generator: minor enhancements

--- trunk/scripts/generator.php
+++ trunk/scripts/generator.php
@@ -32,6 +32,8 @@
 //////////////////////////////////////////////////////////////////////////////////
 // }}}
 
+exec('rm -rf app/');
+
 error_reporting(E_ALL);
 
 // main TOP_LOCATION.
@@ -65,14 +67,14 @@ $config = Configurator::factory('XML', TOP_LOCATION . 'config' . DIRECTORY_SEPAR
 
 if(VERBOSE) echo "Configurator loaded..........[ OK ]\n";
 
-echo "......[ OK ]\n";
+echo "............[ OK ]\n";
 
 print_header('Scaffolding Todo Model');
 include_once('creole/Creole.php');
-
+include_once('active/support/Inflector.php');
 try {
     if(VERBOSE) echo "Establish a DB Connection";
-    Creole::getConnection($config->getDatabaseDsn());
+    $conn = Creole::getConnection($config->getDatabaseDsn());
 } catch (SQLException $sqlEx) {
     echo "..........[ FAILED ]\n";
     done ($sqlEx->getMessage, 255);
@@ -81,24 +83,25 @@ if(VERBOSE) echo "..........[ OK ]\n";
 
 
 $model = 'todo';
-
 $Umodel = ucfirst($model);
-
-
+$models = Inflector::pluralize($model);
+$table_info = $conn->getDatabaseInfo()->getTable($models);
+$pk = $table_info->getPrimaryKey()->getName();
 $date = strftime("%d/%m/%Y %H:%M:%S", time());
+$pattern = '/^(.*)_id$/';
 
 $startscript="scripts/generator.php";
 $fileowneruid=fileowner($startscript);
 $fileownerarray=posix_getpwuid($fileowneruid);
 $user=$fileownerarray['name'];
 
-echo " -- creating ApplicationController.....";
 $app_php = $config->getProperty('application_path') . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . 'application.php';
+echo " -- creating ApplicationController: " . $app_php;
 if (is_file($app_php)) {
     echo ".....[ FILE EXISTS - SKIPING ]\n";
 } else {
     if (!is_dir($config->getProperty('application_path'))) {
-        generate_application_path($config->getProperty('application_path'));
+        generate_application_skeleton($config->getProperty('application_path'));
     }
     $_app = file_get_contents('scripts/templates/application.txt');
     $s1 = str_replace('${__USER__}', $user, $_app);
@@ -107,8 +110,8 @@ if (is_file($app_php)) {
     echo ".....[ OK ]\n";
 }
 
-echo " -- creating " . $Umodel . " Model.....";
 $file = $config->getProperty('application_path') . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . $model . '.php';
+echo " -- creating " . $Umodel . " Model: " . $file;
 if (is_file($file)) {
     echo ".....[ FILE EXISTS - SKIPING ]\n";
 } else {
@@ -120,8 +123,8 @@ if (is_file($file)) {
     echo ".....[ OK ]\n";
 }
 
-echo " -- creating " . $model . " Helper.....";
 $file = $config->getProperty('application_path') . DIRECTORY_SEPARATOR . 'helpers' . DIRECTORY_SEPARATOR . $model . '_helper.php';
+echo " -- creating " . $model . " Helper: " . $file;
 if (is_file($file)) {
     echo ".....[ FILE EXISTS - SKIPING ]\n";
 } else {
@@ -133,8 +136,8 @@ if (is_file($file)) {
     echo ".....[ OK ]\n";
 }
 
-echo " -- creating " . $model . " Controller.....";
 $file = $config->getProperty('application_path') . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . $model . '_controller.php';
+echo " -- creating " . $model . " Controller: " . $file;
 if (is_file($file)) {
     echo ".....[ FILE EXISTS - SKIPING ]\n";
 } else {
@@ -143,30 +146,86 @@ if (is_file($file)) {
     $s2 = str_replace('${__DATE__}', $date, $s1);
     $s3 = str_replace('${model}', $model, $s2);
     $s4 = str_replace('${Model}', $Umodel, $s3);
-    file_put_contents($file, $s4);
+    $s5 = str_replace('${models}', $models, $s4);
+    file_put_contents($file, $s5);
     echo ".....[ OK ]\n";
 }
 
-function generate_application_path($path) {
-    $top = $path . DIRECTORY_SEPARATOR;
-    if (!mkdir($top . 'controllers', 0755, TRUE)) {
-        done(".....[ FAILED ]\nCannot create " . $top . "controllers folder.", 255);
-    }
-    if (!mkdir($top . 'helpers',0755)) {
-        done(".....[ FAILED ]\nCannot create " . $top . "helpers folder.", 255);
-    }
-    if (!mkdir($top . 'models',0755)) {
-        done(".....[ FAILED ]\nCannot create " . $top . "models folder.", 255);
-    }
-    if (!mkdir($top . 'views',0755)) {
-        done(".....[ FAILED ]\nCannot create " . $top . "views folder.", 255);
-    }
+$views_dir = $config->getProperty('application_path') . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $model;
+if (!is_dir($views_dir)) {
+    mkdir($views_dir, 0755);
 }
 
+// ------------------------- ALL
+$file = $views_dir . DIRECTORY_SEPARATOR . 'all.phtml';
+echo " -- creating all " . $model . " view: " . $file;
+if (is_file($file)) {
+    echo ".....[ FILE EXISTS - SKIPING ]\n";
+} else {
+    $_app = file_get_contents('scripts/templates/all.txt');
+    $s1 = str_replace('${__USER__}', $user, $_app);
+    $s2 = str_replace('${__DATE__}', $date, $s1);
+
+    $buff = '<br />';
+
+    foreach( $table_info->getColumns() as $col) {
+        if ($col->getName() == $pk) continue;       
+        if ( preg_match($pattern, $col->getName(), $matches) ) continue;
+        $buff .= ucfirst($col->getName()) . ": <?=\$${model}->" . $col->getName() . ";?><br />";
+    }
+    
+    // $buff = substr($buff, 0, -4) . "<br />";
+    
+    $s21 = str_replace('${__BUFFER__}', $buff, $s2);
+    
+    $s3 = str_replace('${model}', $model, $s21);
+    $s4 = str_replace('${Model}', $Umodel, $s3);
+    $s5 = str_replace('${models}', $models, $s4);
+    $s6 = str_replace('${__PK__}', $pk, $s5);
+    
+    file_put_contents($file, $s6);
+    echo ".....[ OK ]\n";
+}
+
+// ------------------------- ANEW
+$file = $views_dir . DIRECTORY_SEPARATOR . 'anew.phtml';
+echo " -- creating all " . $model . " view: " . $file;
+if (is_file($file)) {
+    echo ".....[ FILE EXISTS - SKIPING ]\n";
+} else {
+    $_app = file_get_contents('scripts/templates/anew.txt');
+    $s1 = str_replace('${__USER__}', $user, $_app);
+    $s2 = str_replace('${__DATE__}', $date, $s1);
+    $s3 = str_replace('${model}', $model, $s2);
+    $s4 = str_replace('${Model}', $Umodel, $s3);
+    $s5 = str_replace('${models}', $models, $s4);
+    
+    $buff = '';
+    
+    // $table_info = $conn->getDatabaseInfo()->getTable($models);
+    // $pk = $table_info->getPrimaryKey()->getName();
+    
+    foreach( $table_info->getColumns() as $col) {
+        $name = $col->getName();
+        $size = $col->getSize();
+        $type = CreoleTypes::getCreoleName($col->getType());
+    }
+    
+    
+    file_put_contents($file, $s5);
+    echo ".....[ OK ]\n";
+}
+
+function generate_application_skeleton($path) {
+    $top = $path . DIRECTORY_SEPARATOR;
+    mkdir($top . 'controllers', 0755, TRUE);
+    mkdir($top . 'helpers',0755);
+    mkdir($top . 'models',0755);
+    mkdir($top . 'views',0755);
+}
 function print_header($text) {
     echo "======> $text.\n";
 }
-
 function done ($message = '', $error_code = 0) {
     echo $message . "\n";
     exit ($error_code);
