2583578b85c292f22c6cd4cfbee30f9b5266df7d
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2005-07-17 13:13:57 +0000

    - added updater script

---
+++ trunk/scripts/updater.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# $Id
+
+mkdir -p vendor
+cd vendor
+echo 'Script for updating/downloading Medick req. from CVS'
+
+echo '===================================================='
+echo 'creole.....'
+echo '[ HINT ] Type << guest >> as password...'
+cvs -d:pserver:guest@cvs.tigris.org:/cvs login
+echo 'checking out creole'
+cvs -d:pserver:guest@cvs.tigris.org:/cvs co -d creole/ creole/creole/classes/creole
+echo '===================================================='
+echo ' simpletest.....'
+echo '[ HINT ] Press << enter >> for password'
+cvs -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/simpletest login
+echo 'checking out simpletest.....'
+cvs -z3 -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/simpletest co -d simpletest -P simpletest/
+
+echo '<======== Updater [ DONE ]'
+cd ../
+
