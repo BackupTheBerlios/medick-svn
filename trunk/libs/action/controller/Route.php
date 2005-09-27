5c8b99cf5c3a92c3f90095c65c904fdbf0f6b6f0
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2005-09-27 18:46:14 +0000

    - refactored to make use of our new Mapping system

--- trunk/libs/action/controller/Route.php
+++ trunk/libs/action/controller/Route.php
@@ -7,13 +7,13 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 //
-//   * Redistributions of source code must retain the above copyright notice, 
-//   this list of conditions and the following disclaimer. 
+//   * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
 //   * Redistributions in binary form must reproduce the above copyright notice,
-//   this list of conditions and the following disclaimer in the documentation 
-//   and/or other materials provided with the distribution. 
-//   * Neither the name of locknet.ro nor the names of its contributors may 
-//   be used to endorse or promote products derived from this software without 
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//   * Neither the name of locknet.ro nor the names of its contributors may
+//   be used to endorse or promote products derived from this software without
 //   specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
@@ -26,96 +26,80 @@
 // CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 // $Id$
-// 
+//
 // ///////////////////////////////////////////////////////////////////////////////
 // }}}
 
-include_once('action/controller/route/MedickRoute.php');
+// include_once('action/controller/route/MedickRoute.php');
 include_once('action/controller/route/RouteException.php');
 
 /**
  * @package locknet7.action.controller.route
  */
 class ActionControllerRoute {
-    
+
     public static function recognize(Request $request) {
+        // XXX. if failed?
         $controller= $request->getParam('controller');
+        // XXX. if failed?
         $action    = $request->getParam('action');
-        
         $map= Map::getInstance();
-        
-        if ($route= $map->contains(new _Route($controller, $action))) {
-            $params= $route->getParams();
-            foreach ($params AS $param) {
-                if (!$request->hasParam($param->getName()) or ($request->getParam($param->getName()) =='')) {
-                    // load failure due to missing parameters.
+        if ($route= $map->contains(new Route($controller,$action))) {
+            foreach ($route->getParams() AS $param) {
+                if (!$request->hasParam($param->getName()) OR ($request->getParam($param->getName()) =='')) {
+                    // XXX. load failure due to missing parameters.
+                    $route= $map->getRouteByName($route->getFailure());
+                    // XXX. failure message.
+                    $request->setParam('controller', $route->getController());
+                    $request->setParam('action', $route->getAction());
+                    break;
+                    // throw new RouteException('Route failed due to the missing parameters!');
+
+                }
+                if ($param->hasValidators()) {
+                    foreach($param->getValidators() AS $validator) {
+                        $validator->setValue($request->getParam($param->getName()));
+                        if (!$validator->validate()) {
+                          // XXX. validation failed.
+                          throw new RouteException('Route validation failed!');
+                        }
+                    }
                 }
             }
-            
+            return self::createController($route);
         }
-        
+        // XXX. no exception here, default rout must be loaded.
+        throw new RouteException('Our map do not contain this Route!');
     }
-    
+
     /**
      * Creates an instance of the requested Controller
-     * In case the route fails, we try to load the default controller specified in the config file 
+     * In case the route fails, we try to load the default controller specified in the config file
      * @param Request, request, the request
      * @return ActionController
      * @throws RouteException
-     * TODO: refactor.
      */
-    public static function createController(Request $request) {
-        $controller = $request->getParam('controller');
-        $passed     = 0;
-        do {
-            if ($passed == 1) {
-                $controller = Configurator::getInstance()->getDefaultRoute()->controller;
-                $request->setParam('controller', $controller);
-            } elseif ($passed == 2) {
-                throw new RouteException('Cannot create a Controller...');
-            }
-            $route = new MedickRoute($controller);
-            $passed++;
-        } while (!self::exists($route));
-
-        $request->setRoute($route);
-        Logger::getInstance()->debug('Running on controller: ' . $controller);
-        include_once($route->getControllerPath() . 'application.php');
-        include_once($route->getControllerPath() . $route->getControllerFile());
-        
+    private static function createController(Route $route) {
+        // this will fail in ReflectionClass.
+        @include_once($route->getControllerPath() . 'application.php');
+        @include_once($route->getControllerPath() . $route->getControllerFile());
+        // XXX. try/catch here.
         $controller_class = new ReflectionClass($route->getControllerName());
-        // start inspection.
-        if ( 
-            ($controller_class->isInstantiable()) 
-            AND 
+
+        if (
+            ($controller_class->isInstantiable())
+            AND
             (
                 ($controller_class->getParentClass()->name=='ApplicationController')
                 OR
                 ($controller_class->getParentClass()->name=='ActionControllerBase')
-            ) 
+            )
             )
         {
             return $controller_class->newInstance();
         }
         throw new RouteException ('Cannot create Controller...');
-    }
-
-    /**
-     * A nice method to check if the given Route Exists.
-     * <code>
-     *      ActionControllerRoute::exists(new MedickRoute($request->getController());
-     * </code>
-     * @access public
-     * @param Route, route, the route
-     * @return bool, true if the controller exists false otherwise.
-     */
-    public static function exists(IRoute $route) {
-        if ($route->getControllerName() === NULL)
-            return FALSE;
-        if (!is_file($route->getControllerPath() . $route->getControllerFile())) 
-            return FALSE;
-        return TRUE;
     }
 }
