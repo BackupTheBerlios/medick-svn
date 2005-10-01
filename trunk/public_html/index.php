ed6ddef296029a83e83cbec70ed0558f72985f36
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2005-10-01 15:51:21 +0000

    - [index.php] medick* renaming

--- trunk/public_html/index.php
+++ trunk/public_html/index.php
@@ -39,5 +39,5 @@
 
 include_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config'. DIRECTORY_SEPARATOR . 'application.bootstrap.php');
 
-MedickDispatcher::dispatch();
+Dispatcher::dispatch();
 
