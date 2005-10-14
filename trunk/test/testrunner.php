c3b98a77bc24e4e14c5e764a053e2e0d914e3037
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2005-10-14 18:03:36 +0000

    -[ test/runner.php ] removed bloated class

--- trunk/test/testrunner.php
+++ trunk/test/testrunner.php
@@ -11,12 +11,12 @@ define('TOP_LOCATION', dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTOR
 
 set_include_path( TOP_LOCATION . 'libs' . DIRECTORY_SEPARATOR . PATH_SEPARATOR .
                   TOP_LOCATION . 'vendor' . DIRECTORY_SEPARATOR . PATH_SEPARATOR . 
-                  TOP_LOCATION . 'app'  . DIRECTORY_SEPARATOR . 'models' . DIRECTORY_SEPARATOR . PATH_SEPARATOR .
                   dirname(__FILE__) 
                 );
 
 error_reporting(E_ALL);
 set_time_limit(0);
+ini_set('display_errors', 1);
 
 include_once('medick/Object.php');
 include_once('medick/Exception.php');
@@ -27,53 +27,34 @@ include_once('simpletest/web_tester.php');
 include_once('simpletest/unit_tester.php');
 include_once('simpletest/reporter.php');
 
+if (is_file('test.db')) unlink('test.db');
 
-// {{{ RecursiveTestLoader
-// loads the test
-// TODO: refactor.
-class RecursiveTestLoader extends RecursiveIteratorIterator {
+$query=<<<__
+    CREATE TABLE todos (
+        id INTEGER PRIMARY KEY,
+        description VARCHAR(100),
+        done INTEGER(1)
+    );
+__;
+sqlite_query(sqlite_open('test.db'), $query);
 
-    private static $test;
-	
-    /** Constructor */
-	function __construct() {
-		parent::__construct(
-		  new CachingRecursiveIterator(
-		      new RecursiveDirectoryIterator(
-		          dirname(__FILE__)
-		      ), 
-		      CIT_CALL_TOSTRING|CIT_CATCH_GET_CHILD
-		  ),
-		1);   
-		self::$test= new GroupTest('====== Routes :: Medick Framework Unit Tests =====');
-        }
+include_once('mock/MockConfigurator.php');
+Registry::put(new MockConfigurator(), '__configurator');
+include_once('logger/Logger.php');
+Registry::put(new Logger(), '__logger');
 
-	/** @return the current element valid element */
-	function current() {
-		$tree = '';
-		for ($l=0; $l < $this->getDepth(); $l++) {
-			$tree .= $this->getSubIterator($l)->hasNext() ? '| ' : '  ';
-		}
-        if (preg_match("/Test.php/i", $this->key()) && is_file($this->key()) && !preg_match("/.svn/i", $this->key())) {
-            self::$test->addTestFile($this->key());
-        }
-	}
+$test= new GroupTest('====== Medick Framework Unit Tests =====');
 
-	/** Aggregates the inner iterator */
-	function __call($func, $params) {
-		return call_user_func_array(array($this->getSubIterator(), $func), $params);
-	}
-	
-	public static function getTest() {
-	   return self::$test;
-	}
-	
+$it = new DirectoryIterator(dirname(__FILE__));
+foreach($it as $file) {
+    if ($file->isDir()) continue;
+    if (preg_match("/Test.php/i", $file->getFileName())) $test->addTestFile($file->getFileName());
 }
 
-foreach(new RecursiveTestLoader() as $test) { }
-
-RecursiveTestLoader::getTest()->run(new TextReporter());
+$test->run(new TextReporter());
 
 $time_end = microtime(true);
 echo "Done in " . ($time_end - $time_start) . " seconds\n";
+
+unlink('test.db');
 
