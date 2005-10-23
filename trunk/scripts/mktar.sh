5b4453742600fb51d5591518c656d2a545fbf21d
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2005-10-23 08:53:39 +0000

    - scripts: mktar.sh a script that builds a tar file from our svn tree

---
+++ trunk/scripts/mktar.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# mktar.sh - create a tar file from a subversion working copy
+# Based on http://svnpkg.tigris.org/source/browse/*checkout*/svnpkg/trunk/mktar/usr/lib/svnpkg/mktar.sh?content-type=text%2Fplain&rev=2
+# Original Copyright 2004-2005 Dick Marinus <dick.marinus@etos.nl>
+# $Id$
+
+echo 'MkTAR, create a tar file from a SVN working copy'
+PW=`pwd`
+C_DATE=`date +%F`
+mkdir -p ~/tmp/medick
+cd ~/tmp/medick
+echo 'Checking out trunk/medick...'
+svn checkout -q svn://svn.berlios.de/medick/trunk medick
+echo 'Done.'
+TARNAME=medick
+TMPDIR=~/tmp/mktar-$(id -un)-$$/
+TMPFILE=~/tmp/mktar-$(id -un)-$$-file
+# cleanup
+rm -rf "${TMPDIR}" "${TMPFILE}"
+mkdir -p ${TMPDIR}/${TARNAME}
+cd medick
+echo 'Removing .svn folders...'
+# concat workingcopies together
+find . ! -regex '.*/\.svn.*' -exec sh -c "
+    if [ -d "{}" ] ; then
+        mkdir -p "${TMPDIR}/${TARNAME}/{}"
+    else
+        DIRNAME=$(dirname {})
+        mkdir -p "${TMPDIR}/${TARNAME}/${DIRNAME}"
+        cat "${TMPDIR}/${TARNAME}/{}" "{}" > $TMPFILE 2>/dev/null
+        mv "${TMPFILE}" "${TMPDIR}/${TARNAME}/{}"
+    fi
+" \;
+cd - > /dev/null
+echo 'Done.'
+echo "Creating tar.gz file: ${TARNAME}-latest.tar.gz"
+tar czf ${TARNAME}.tar.gz -C "${TMPDIR}/" ${TARNAME}
+mv ${TARNAME}.tar.gz ${PW}/${TARNAME}-latest.tar.gz
+cd $PW
+echo 'Done.'
+echo 'Cleaning-up...'
+#clean up
+rm -rf "${TMPDIR}" "${TMPFILE}"
+rm -rf ~/tmp/medick
+echo 'MkTAR Done.'
