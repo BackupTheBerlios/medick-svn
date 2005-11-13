6777ae84ae156cda3e35773ee628dfc51cd82099
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2005-11-13 17:06:54 +0000

    - [ eltodo.db.schema ] added project table

--- applications/eltodo/db/schema.sql
+++ applications/eltodo/db/schema.sql
@@ -1,7 +1,22 @@
-DROP TABLE IF EXISTS `todos`;
-CREATE TABLE IF NOT EXISTS `todos` (
-  `id` int(11) NOT NULL auto_increment,
-  `description` varchar(100) NOT NULL default '',
-  `done` tinyint(4) NOT NULL default '0',
-  PRIMARY KEY  (`id`)
+-- ------------------------
+-- eltodo database schema.
+-- $Id$
+-- ------------------------
+
+DROP TABLE IF EXISTS projects;
+CREATE TABLE projects (
+	id 	int(11) 	not null auto_increment,
+	name 	varchar(255) 	not null,
+	primary key (id)
 );
+
+DROP TABLE IF EXISTS todos;
+CREATE TABLE IF NOT EXISTS todos (
+	id 		int(11) 	not null auto_increment,
+	project_id	int(11)		not null,
+	description 	varchar(100) 	not null default '',
+	done 		tinyint(4) 	not null default '0',
+	constraint fk_todos_project foreign key (project_id) references projects(id),
+	primary key (id)
+);
+
