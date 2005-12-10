05493b950a9bdc79bbcdc0bc6854bc2b3d4f7202
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2005-12-10 16:07:13 +0000

    -- including medick.util when booting.

--- trunk/boot.php
+++ trunk/boot.php
@@ -7,13 +7,13 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 //
-//   * Redistributions of source code must retain the above copyright notice, 
-//   this list of conditions and the following disclaimer. 
+//   * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
 //   * Redistributions in binary form must reproduce the above copyright notice,
-//   this list of conditions and the following disclaimer in the documentation 
-//   and/or other materials provided with the distribution. 
-//   * Neither the name of locknet.ro nor the names of its contributors may 
-//   be used to endorse or promote products derived from this software without 
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//   * Neither the name of locknet.ro nor the names of its contributors may
+//   be used to endorse or promote products derived from this software without
 //   specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
@@ -26,9 +26,9 @@
 // CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 // $Id$
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 // }}}
 
@@ -54,13 +54,14 @@ set_error_handler(array(new ErrorHandler(), 'raiseError'));
 include_once('medick/Registry.php');
 include_once('medick/Dispatcher.php');
 include_once('medick/Version.php');
+include_once('medick/util.php');
 include_once('configurator/XMLConfigurator.php');
 include_once('logger/Logger.php');
 include_once('action/controller/Map.php');
 
-$conf_files = $_SERVER['MEDICK_APPLICATION_PATH'] . DIRECTORY_SEPARATOR . 'conf' . 
+$conf_files = $_SERVER['MEDICK_APPLICATION_PATH'] . DIRECTORY_SEPARATOR . 'conf' .
                         DIRECTORY_SEPARATOR . $_SERVER['MEDICK_APPLICATION_NAME'];
-                        
+
 Registry::put(new XMLConfigurator($conf_files . '.xml'), '__configurator');
 
 $map= Registry::put(new Map(), '__map');
