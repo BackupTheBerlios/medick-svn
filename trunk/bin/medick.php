1749f44a987309a1744963ba12c69b30b851aa4a
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2005-12-23 15:02:40 +0000

    -- more files are generated

--- trunk/bin/medick.php
+++ trunk/bin/medick.php
@@ -64,7 +64,6 @@ $files= array(
   'public_html' . DIRECTORY_SEPARATOR . 'default.htaccess'  => 'public' . DIRECTORY_SEPARATOR . '.htaccess',
   'config' . DIRECTORY_SEPARATOR . 'application.xml'        => 'conf'   . DIRECTORY_SEPARATOR . $app_name.'.xml',
   'config' . DIRECTORY_SEPARATOR . 'application.routes.php' => 'conf'   . DIRECTORY_SEPARATOR . $app_name.'.routes.php'
-  
 );
 
 $search= array(
@@ -87,9 +86,50 @@ foreach ($files as $from=>$file) {
     write_file($contents, $app_location.DIRECTORY_SEPARATOR.$folders[$to].DIRECTORY_SEPARATOR.$orig);
 }
 
+// plain files.
 write_file(' ',$app_location.DIRECTORY_SEPARATOR.$folders['log'].DIRECTORY_SEPARATOR.$app_name.'.log',0777);
 
-write_file("<html><head><title>Welcome to Medick!</title></head><body><h3>Welcome To Medick</h3><p><b>Application:</b> {$app_name}</p><p><b>Location: </b>{$app_location}</p></body></html>", $app_location.DIRECTORY_SEPARATOR.$folders['public'].DIRECTORY_SEPARATOR.'index.html');
+$schema_sql=<<<EOSQL
+-- \$Id $
+-- Database Schema for $app_name
 
-            
+EOSQL;
+
+$index_html=<<<EOHTML
+<html>
+  <head>
+    <!-- \$Id$ -->
+    <title>Welcome to Medick!</title>
+  </head>
+  <body>
+    <h3>Welcome To Medick</h3>
+    <p><b>Application:</b> $app_name</p>
+    <p><b>Location: </b>$app_location</p>
+  </body>
+</html>
+
+EOHTML;
+
+
+$application_controller=<<<EOCLASS
+<?php
+
+  /**
+   * This class is part of $app_name project
+   *
+   * Methods added here will be available in all your controllers.
+   * \$Id$
+   * @package $app_name.controllers
+   */
+  class ApplicationController extends ActionControllerBase {
+
+  }
+    
+EOCLASS;
+
+write_file($index_html, $app_location.DIRECTORY_SEPARATOR.$folders['public'].DIRECTORY_SEPARATOR.'index.html');
+
+write_file($application_controller, $app_location.DIRECTORY_SEPARATOR.$folders['controllers'].DIRECTORY_SEPARATOR.'application.php');
+
+write_file($schema_sql,$app_location.DIRECTORY_SEPARATOR.$folders['db'].DIRECTORY_SEPARATOR.'schema.sql');
 
