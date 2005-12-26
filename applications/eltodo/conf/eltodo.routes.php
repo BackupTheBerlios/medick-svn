93cae74e5032d275e4ec715b0e86cb07ba904a54
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2005-12-26 15:59:11 +0000

    -- using the new routing system in eltodo sample application

--- applications/eltodo/conf/eltodo.routes.php
+++ applications/eltodo/conf/eltodo.routes.php
@@ -2,26 +2,7 @@
 // eltodo.routes.php
 // $Id$
 
-{
-    { // solves: /, /project.html, /project/index.html
-        $project= new Route('project');
-        $project->setAction('all');
-        $project->setName('default');
-        $map->add($project);
-    }
-    
-    { // solves /project/create.html
-        $create_project= new Route('project', 'create');
-        $create_project->add(new RouteParam('name'));
-        $create_project->setFailure($project);
-        $map->add($create_project);
-    }
-    
-    { // solves: /project/delete/{id}.html
-        $delete_project= new Route('project', 'delete');
-        $delete_project->add(new RouteParam('id'));
-        $delete_project->setFailure($project);
-        $map->add($delete_project);
-    }
-}
+$map= Map::getInstance();
+$map->add(new Route(':controller/:action/:id'));
+
 ?>
