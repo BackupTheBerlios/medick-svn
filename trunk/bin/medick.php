92b2befa064f64ed68fa39938292d3af231a3e3e
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2006-01-06 20:06:06 +0000

    -- fix: using the last part of the passed PATH to application as the application name

--- trunk/bin/medick.php
+++ trunk/bin/medick.php
@@ -20,7 +20,7 @@ function write_file($contents, $to, $mode= FALSE) {
     if (file_exists($to)) {
         echo "\texists {$to}\n";
     } else {
-        if(file_put_contents($to, $contents)) {
+        if(@file_put_contents($to, $contents)) {
             echo "\tcreate {$to}\n";
         } else {
             exit("Cannot create {$to}, permissions?\n");
@@ -51,7 +51,12 @@ $folders= array(
 
 $app_name= isset($argv[1]) ? $argv[1] : exit("No Application Location Specified.\n");
 
+$x = explode(DIRECTORY_SEPARATOR,$app_name); $short_name = end($x);
+
 $app_location= getcwd() . DIRECTORY_SEPARATOR . $app_name;
+
+echo "Creating application: $short_name\n";
+echo "Location:\n\t$app_location\n\n";
 
 mk_dir($app_location);
 
@@ -65,9 +70,9 @@ $files= array(
   'skel' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'default.htaccess'  
                               => 'public' . DIRECTORY_SEPARATOR . '.htaccess',
   'skel' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'application.xml'        
-                              => 'conf'   . DIRECTORY_SEPARATOR . $app_name.'.xml',
+                              => 'conf'   . DIRECTORY_SEPARATOR . $short_name.'.xml',
   'skel' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'application.routes.php' 
-                              => 'conf'   . DIRECTORY_SEPARATOR . $app_name.'.routes.php'
+                              => 'conf'   . DIRECTORY_SEPARATOR . $short_name.'.routes.php'
 );
 
 $search= array(
@@ -91,7 +96,7 @@ foreach ($files as $from=>$file) {
 }
 
 // plain files.
-write_file(' ',$app_location.DIRECTORY_SEPARATOR.$folders['log'].DIRECTORY_SEPARATOR.$app_name.'.log',0777);
+write_file(' ',$app_location.DIRECTORY_SEPARATOR.$folders['log'].DIRECTORY_SEPARATOR.$short_name.'.log',0777);
 
 $schema_sql=<<<EOSQL
 -- \$Id$
@@ -107,9 +112,9 @@ $index_html=<<<EOHTML
   </head>
   <body>
     <h3><center>Welcome To Medick</center></h3>
-    <p><b>Application:</b> $app_name</p>
+    <p><b>Application:</b> $short_name</p>
     <p><b>Develop $app_name on : </b>$app_location</p>
-    <p>Setup a default controller in <i>$app_location/config/$app_name.routes.php</i>, and remove this file.</p>
+    <p>Setup a default controller in <i>$app_location/config/$short_name.routes.php</i>, and remove this file.</p>
     <p>Ask for support on medick <a href="https://lists.berlios.de/mailman/listinfo/medick-devel">development list</a>.</p>
   </body>
 </html>
@@ -121,7 +126,7 @@ $application_controller=<<<EOCLASS
 <?php
 
   /**
-   * This class is part of $app_name project
+   * This class is part of $short_name project
    *
    * Methods added here will be available in all your controllers.
    * \$Id$
@@ -138,4 +143,6 @@ write_file($index_html, $app_location.DIRECTORY_SEPARATOR.$folders['public'].DIR
 write_file($application_controller, $app_location.DIRECTORY_SEPARATOR.$folders['controllers'].DIRECTORY_SEPARATOR.'application.php');
 
 write_file($schema_sql,$app_location.DIRECTORY_SEPARATOR.$folders['db'].DIRECTORY_SEPARATOR.'schema.sql');
+
+echo "\nmedick (\$v:" . str_replace("\n","",file_get_contents($medick_core.DIRECTORY_SEPARATOR.'VERSION')) . ") done.\n";
 
