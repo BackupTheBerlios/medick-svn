943d70eb53271addf6bef087e83b2da75a13c44b
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2007-08-28 14:29:29 +0000

    -- updated README + more mocks

--- exp/ar5/test/mock/driver.php
+++ exp/ar5/test/mock/driver.php
@@ -7,6 +7,10 @@ include_once('active/record/drivers/abstract/SQLPreparedStatement.php');
 include_once('active/record/drivers/abstract/SQLResultSet.php');
 include_once('active/record/drivers/abstract/SQLTableInfo.php');
 
+class MocTableInfo extends SQLTableInfo {
+
+}
+
 class MockConnection extends SQLConnection {
   
   public function connect(Array $dsn=array()) {
@@ -23,6 +27,26 @@ class MockConnection extends SQLConnection {
   
   public function getLastErrorMessage() {
     return 'mock.';
+  }
+
+  public function getTableInfo($name, $force=false) {
+    return new MockTableInfo($name, $this);
+  }
+
+  public function execute( $sql ) {
+
+  }
+
+  public function exec( $sql ) {
+
+  }
+
+  public function getUpdateCount($rs=null) {
+
+  }
+
+  public function prepare( $sql ) {
+    return new MockPreparedStatement($sql, $this);
   }
   
 }
