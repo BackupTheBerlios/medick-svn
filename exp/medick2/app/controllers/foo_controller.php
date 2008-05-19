bb65fec0ae1c9496b00593e64bee6c8f0132d9b1
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2008-05-19 08:10:10 +0000

    -- more on medick2 exp.

--- exp/medick2/app/controllers/foo_controller.php
+++ exp/medick2/app/controllers/foo_controller.php
@@ -6,4 +6,12 @@ class ApplicationController extends ActionController {
 
 class FooController extends ApplicationController {
 
+  public function bar($arg1, $arg2, $arg3='bar') {
+    // Medick::dump( $arg3 );
+  }
+
+  public function mur() {
+    $this->template->assign('time', time());
+  }
+
 }
