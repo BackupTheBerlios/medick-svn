cf1abbd9409caa3f7ba8e1eef6b5b6e0ef6c9f8e
aurelian <aurelian@e651dfcc-a6f9-0310-9ee8-f0874f5ef385>
2008-05-26 17:07:39 +0000

    -- I haz karma

--- exp/medick2/vendor/medick/__remove_me/runkit/runkit.php
+++ exp/medick2/vendor/medick/__remove_me/runkit/runkit.php
@@ -9,14 +9,19 @@ include_once('foo.php');
 $php1= new Runkit_Sandbox();
 $php1->eval( "include_once('foo.php');Foo::bar();" );
 
-echo "Global Scope: " . Foo::$baz . "--->0\n";
+echo "Global Scope: [" . Foo::$baz . "] ---> 0\n";
 
 $php2= new Runkit_Sandbox();
-$php2['parent_call'] = true;
-$php2->eval('include_once("foo.php");');
-$php2->eval('Foo::bar();');
-$php2->eval('echo "PHP2 Scope: " . Foo::$baz . " --->1\n";');
 
-$php2->eval('echo my_func();');
+$php2->eval('include_once("foo.php");');
+$php2->eval('$karma = 15;');
+$php2->eval('Foo::bar();');
+$php2->eval('echo "PHP2 Scope: [" . Foo::$baz . "] ---> 1\n";');
+
+echo "Getting karma out: [" . $php2->karma . "] ---> 15\n";
+$php2->eval('$karma++;');
+$php2->eval('echo "increased karma: [". $karma ."] ---> 16\n";');
+
+// $php2->eval('echo my_func();');
 
 
